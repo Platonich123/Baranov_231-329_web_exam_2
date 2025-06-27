577145161f0ae2c4c3a507cb3b625c5b4862915d
Platonich123 <145435938+Platonich123@users.noreply.github.com>
2025-06-27 16:00:23 +0300

    Update main.js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,10 +1,7 @@
-// Ваши JS-скрипты
-// Инициализация Bootstrap-модальных окон (если требуется)
 document.addEventListener('DOMContentLoaded', function() {
     var modals = document.querySelectorAll('.modal');
     modals.forEach(function(modal) {
         modal.addEventListener('show.bs.modal', function (event) {
-            // Можно добавить логику при открытии модального окна
         });
     });
 }); 
